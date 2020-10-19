387988244156ba85673aa2082fd7d138a686b4da
Nick <fireblade.nicholas@hotmail.com>
2020-10-19 09:06:46 -0400

    fix

--- WoWRC/settings.py
+++ WoWRC/settings.py
@@ -113,22 +113,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': (os.path.join(os.path.dirname(__file__), '..', 'templates').replace('\\', '/'),),
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages'
-            ],
-        },
-    },
-]
-
 # REST framework
 
 REST_FRAMEWORK = {

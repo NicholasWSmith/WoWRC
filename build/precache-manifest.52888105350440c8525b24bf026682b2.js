379b5fe686db129e6edbc13afa33a4050c975373
Nick <fireblade.nicholas@hotmail.com>
2020-10-19 09:14:36 -0400

    add build

---
+++ build/precache-manifest.52888105350440c8525b24bf026682b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530c54ef4648f0e2f03d0acb78a6e937",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfa6038864c1f8094c70",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa6038864c1f8094c70",
+    "url": "/static/js/main.23169dfd.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

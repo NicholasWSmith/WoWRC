27c90ac5828ec2a1eda2b7d6eee2afd601a6af57
Nick <fireblade.nicholas@hotmail.com>
2020-11-23 11:05:04 -0500

    add comments

--- auth_users/busi_logic.py
+++ auth_users/busi_logic.py
@@ -12,15 +12,19 @@ def calculate_paid_total(runs):
         ids.append(run['boostersid']['DPS'][0]['boosterid'])
         ids.append(run['boostersid']['DPS'][1]['boosterid'])
 
+        # Get all the cuts
         booster_cut = float(run['details']['Booster Cut'])
         advertiser_cut = float(run['details']['Advertiser Cut'])
         kh_cut = float(run['details']['Keyholder Cut'])
 
+        # Loop through the ids
         for index, b_id in enumerate(ids):
             if b_id is not None:
+                #  Set inital value if discord user is new to dict
                 if b_id not in keys:
                     data[b_id] = 0.0
 
+                # Add to total based on role
                 if index == 0:
                     data[b_id] += advertiser_cut
                 elif index in [1, 2, 3, 4]:
@@ -28,11 +32,13 @@ def calculate_paid_total(runs):
 
                 data[b_id] = round(data[b_id], 2)
 
+        #  Grab the KH data if it exists, and add to total.
         kh = run.get('Key Holder', None)
 
         if kh is not None:
             kh_id = kh['boosterid']
             if kh_id is not None:
                 data[kh_id] += kh_cut
-
+                
+    # Returns all data.
     return data
